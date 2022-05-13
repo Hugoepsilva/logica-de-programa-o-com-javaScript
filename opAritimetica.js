7e89b0b36956429f430c716f771d6ad20769bb2a
Perkles <hugoewertonheps21@gmail.com>
2022-05-13 10:35:50 -0300

    operadoes aritimeticos com javaScript

---
+++ opAritimetica.js
@@ -0,0 +1,16 @@
+/* 
+faça as quatroa operações basicas com os numeros 2 e 4
+ao final de cada operação impirma na tela nos seguintes formatos
+soma = 2 + 4 = x
+subtração = 2 - 4 = x
+divisao = 2 / 4 = x
+multiplicação = 2 * 4 = z
+*/
+
+let num1 = 2;
+let num2 = 4;
+
+console.log('soma = 2 + 4 = ' + (num1 + num2));
+console.log('subtração = 2 - 4 = ' + (num1 - num2));
+console.log('divisao = 2 / 4 = ' + (num1 / num2));
+console.log('multiplicação = 2 * 4 = ' + (num1 * num2));
